be8175f491c854e4ecdfa3ea99becde95ad32c0e
SevenCats <snowcolton@hotmail.com>
2020-11-05 03:54:27 -0500

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+	root: true,
+	extends: 'airbnb-base',
+	parserOptions: { sourceType: 'script' },
+	env: { node: true },
+	rules: {
+		strict: [2, 'global'],
+		indent: [2, 'tab'],
+		'no-tabs': 0,
+		'arrow-parens': [2, 'as-needed'],
+		'no-console': 0,
+		'func-names': 0,
+		'consistent-return': 0,
+	},
+	overrides: [
+		{
+			files: [
+				'**/__tests__/*.js',
+				'**/__mocks__/*.js',
+				'tests/**/*.spec.js',
+				'tests/util.js',
+				'jest.integration.setup.js',
+				'jest.setup.js',
+			],
+			env: { jest: true },
+			plugins: ['jest'],
+			rules: {
+				camelcase: 0,
+				'import/no-extraneous-dependencies': [2, {
+					devDependencies: true,
+				}],
+			},
+		},
+	],
+};
---
+++ knexfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+require('dotenv').config();
+const knexStringcase = require('knex-stringcase');
+
+module.exports = knexStringcase({
+	client: 'pg',
+	connection: {
+		host: process.env.POSTGRES_HOST,
+		user: process.env.POSTGRES_USER,
+		password: process.env.POSTGRES_PASSWORD,
+		database: process.env.POSTGRES_DB,
+	},
+});
